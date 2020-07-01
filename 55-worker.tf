49c35675d8504bd1d3d39fb0d9305dc53b16816f
nalbam <me@nalbam.com>
2020-07-01 16:43:24 +0900

    terraform-aws-asg?ref=v0.12.24

--- 55-worker.tf
+++ 55-worker.tf
@@ -1,7 +1,7 @@
 # worker
 
 module "worker" {
-  source = "github.com/nalbam/terraform-aws-asg?ref=v0.12.23"
+  source = "github.com/nalbam/terraform-aws-asg?ref=v0.12.24"
   # source = "../terraform-aws-asg"
 
   name = var.name
