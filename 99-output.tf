c37d280f9b22a5a389f2626b4ea289bb3734aa4d
nalbam <me@nalbam.com>
2020-07-07 16:49:08 +0900

    iam_role_arn

--- 99-output.tf
+++ 99-output.tf
@@ -1,16 +1,16 @@
 # output
 
-output "iam_role_arn" {
-  value = module.worker.iam_role_arn
-}
+# output "iam_role_arn" {
+#   value = module.worker.iam_role_arn
+# }
 
-output "iam_role_name" {
-  value = module.worker.iam_role_name
-}
+# output "iam_role_name" {
+#   value = module.worker.iam_role_name
+# }
 
-output "security_group_id" {
-  value = module.worker.security_group_id
-}
+# output "security_group_id" {
+#   value = module.worker.security_group_id
+# }
 
 output "aws_launch_configuration_ids" {
   value = module.worker.aws_launch_configuration_ids
