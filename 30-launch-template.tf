39cb3d33e89f7c2898592dc144c2bcc5ebdff62a
nalbam <me@nalbam.com>
2021-08-12 11:58:57 +0900

    v0.14

---
+++ 30-launch-template.tf
@@ -0,0 +1,51 @@
+# launch_template
+
+resource "aws_launch_template" "worker" {
+  name_prefix = format("%s-", local.worker_name)
+
+  image_id      = local.ami_id
+  instance_type = length(var.mixed_instances) > 0 ? var.mixed_instances[0] : var.instance_type
+
+  user_data = base64encode(local.user_data)
+
+  ebs_optimized = var.ebs_optimized
+  key_name      = var.key_name
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+
+    ebs {
+      volume_type           = var.volume_type
+      volume_size           = var.volume_size
+      delete_on_termination = true
+    }
+  }
+
+  monitoring {
+    enabled = var.enable_monitoring
+  }
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.worker.name
+  }
+
+  network_interfaces {
+    delete_on_termination       = true
+    associate_public_ip_address = var.associate_public_ip_address
+    security_groups             = var.security_groups
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = local.tags
+  }
+
+  tag_specifications {
+    resource_type = "volume"
+
+    tags = local.tags
+  }
+
+  tags = local.tags
+}
