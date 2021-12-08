46f94a7555bbf1ad0a440c2b9e1b675413c1a3b7
nalbam <me@nalbam.com>
2021-12-08 14:11:20 +0900

    add worker_instance_profile_name

--- 03-locals.tf
+++ 03-locals.tf
@@ -17,6 +17,8 @@ locals {
   instance_types = compact(concat([var.instance_type], var.mixed_instances))
 
   ami_id = var.ami_id != "" ? var.ami_id : data.aws_ami.worker.id
+
+  instance_profile_name = var.instance_profile_name != "" ? var.instance_profile_name : aws_iam_instance_profile.worker.name
 }
 
 locals {
